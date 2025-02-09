158fd1648060e384d475e258e505f7d21d5bac47
JhofredCam <jhcamacho@unal.edu.co>
2025-02-09 13:55:37 -0500

    Path & Query

--- Backend/FastApi/users.py
+++ Backend/FastApi/users.py
@@ -6,15 +6,16 @@ app = FastAPI()
 # Entidad user
 
 class User(BaseModel):
+    id: int
     name: str
     surname: str
     url: str
     age: int
 
 users_list = [
-    User(name="Carlos", surname="Hernández", url="https://example.com/carlos-hernandez", age=30),
-    User(name="María", surname="Gómez", url="https://example.com/maria-gomez", age=25),
-    User(name="Luis", surname="Pérez", url="https://example.com/luis-perez", age=35)
+    User(id=1,name="Carlos", surname="Hernández", url="https://example.com/carlos-hernandez", age=30),
+    User(id=2,name="María", surname="Gómez", url="https://example.com/maria-gomez", age=25),
+    User(id=3,name="Luis", surname="Pérez", url="https://example.com/luis-perez", age=35)
 ]
 
 @app.get("/usersjson")
@@ -28,3 +29,20 @@ async def usersjson():
 @app.get("/users")
 async def users():
     return users_list
+
+@app.get("/users/{id}")
+async def user(id: int):
+    try:
+        user = filter(lambda user :  user.id == id, users_list)
+        return list(user)[0]
+    except:
+        return {"Error":"Not valid id"}
+    
+
+@app.get("/user")
+async def user(id: int):
+    try:
+        user = filter(lambda user :  user.id == id, users_list)
+        return list(user)[0]
+    except:
+        return {"Error":"Not valid id"}
